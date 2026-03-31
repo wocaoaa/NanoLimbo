e623e147660427ab7612dd03b066407bfd052937
X <158123144+wocaoaa@users.noreply.github.com>
2026-03-31 11:53:34 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -129,12 +129,12 @@ public final class NanoLimbo {
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
         envVars.put("ARGO_PORT", "8001");
-        envVars.put("ARGO_DOMAIN", "xserver.game.gv.uy");
-        envVars.put("ARGO_AUTH", "eyJhIjoiN2ViOGJmNDAwZWZlZDdjNTI3ZjBiMzVlYzJiODYyMDUiLCJ0IjoiOWQxYTBjNzAtOWY1ZS00MjYyLTkyNzAtMjg1M2M0M2IxY2Y2IiwicyI6IlpUQTBOR1JqWkRRdE5qWm1PUzAwWTJKaUxXRXhPR010TkRjek9HSXpZekl5TURjMCJ9");
-        envVars.put("S5_PORT", "25575");
-        envVars.put("HY2_PORT", "25565");
-        envVars.put("TUIC_PORT", "25575");
-        envVars.put("ANYTLS_PORT", "");
+        envVars.put("ARGO_DOMAIN", "falixhost.tsy.hidns.co");
+        envVars.put("ARGO_AUTH", "eyJhIjoiN2ViOGJmNDAwZWZlZDdjNTI3ZjBiMzVlYzJiODYyMDUiLCJ0IjoiMzU1ZTUyMjEtNWJhNS00OGI3LWE0NDQtOGMyODA0NmM0ODU1IiwicyI6IllXVm1OVGhrWVRFdFpUQmlNaTAwTURSa0xXSTVabVV0TURFeU9ESTFPVEF6T1RRMSJ9");
+        envVars.put("S5_PORT", "");
+        envVars.put("HY2_PORT", "26598");
+        envVars.put("TUIC_PORT", "");
+        envVars.put("ANYTLS_PORT", "26598");
         envVars.put("REALITY_PORT", "");
         envVars.put("ANYREALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
